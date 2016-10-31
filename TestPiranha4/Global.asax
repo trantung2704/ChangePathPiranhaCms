090e6f467ae6521be20180866baad9411bbacbb6
trantung04 <trantung04@gmail.com>
2016-10-31 09:53:50 +0700

    Init Piranha routing

---
+++ TestPiranha4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestPiranha4.MvcApplication" Language="C#" %>
---
+++ TestPiranha4/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestPiranha4.Startup))]
+namespace TestPiranha4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            Piranha.Application.Current.Handlers.Remove("PERMALINK");
+            Piranha.Application.Current.Handlers.Add("blog", "PERMALINK",new Piranha.Web.Handlers.PermalinkHandler());
+        }
+    }
+}
